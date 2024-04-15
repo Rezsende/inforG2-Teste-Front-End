14ed989088f0e4b84a0479c0ba008e9e412afd2b
João Paulo <jpresendejava@gmail.com>
2024-04-15 16:32:30 -0400

    crete header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,22 @@
   <div>
     
 
-    <header>nave</header>
+    <header>
+
+      <div class="buton-toggle">
+        <img class="img-togle" src="../assets/togle.png"/>
+      </div>
+      
+      <div class="route-menu">
+        <div>
+          <span class="settings-label">Utilitarios</span>
+        </div>
+        <div>
+          <span class="Manger">Manager</span>
+        </div>
+      </div>
+
+    </header>
    <div class="body-main">
     <aside>section</aside>
     <main>Main</main>
@@ -56,26 +71,14 @@ axios.get('https://api-manager-test.infog2.com.br.infog2.com.br/a/colaborador/?t
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
 </script>
 
 
 <style scoped>
-
-
 header{ background: #0077FF;
-
 height: 50px;
+display: flex;
+flex: 1;
 
 }
 aside{
@@ -86,14 +89,44 @@ height: calc(100vh - 50px);
 }
 
 .body-main{
-
-  display: flex;
-  
+display: flex;
 }
 main{
-  display: flex;
-  /* width: 100%; */
-  flex: 1;
-  background: #c54f0a;}
+display: flex; 
+flex: 1;
+background: #ffffff;}
 
+
+.buton-toggle{
+display: flex;
+ 
+ width: 56px;
+ justify-content: start;
+ align-items: center;
+ 
+}
+.route-menu{
+  display: flex;
+  flex: 1;
+  align-items: center;
+
+  justify-content: space-between;
+
+}
+
+.img-togle{
+margin-left: 10px;  
+height: 30px;
+}
+
+.Manger{
+  color: #ffffff;
+  margin-right: 20px;
+  font-size: 20px;
+}
+.settings-label{
+  margin-left: 50%;
+  font-size: 20px;
+  color: #ffffff;
+}
 </style>
